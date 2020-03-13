586e20e34e41576310cfbd0a33f5372e837e23dc
hayashi-ay <a_hayashi@codmon.co.jp>
2020-03-13 11:21:14 +0900

    feat: Add login and logout botton

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { domain, clientId } from "../auth_config.json";
+import { Auth0Plugin } from "./auth";
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
 
 Vue.config.productionTip = false
 
